9481a7537b709698dbf61b072d64e086430e2540
fariraimasocha <114150009+fariraimasocha@users.noreply.github.com>
2023-07-09 16:42:46 +0200

    Fix! Cart Logo

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -21,6 +21,16 @@
                             {{ __('Products') }}
                         </x-nav-link>
 
+                        <a href="{{ route('shopping.cart') }}" class="relative mt-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 14h6m2.5 0h2.334a.5.5 0 0 1 .464.321l2.302 6.907A1 1 0 0 1 21.802 22H2.198a1 1 0 0 1-.96-1.772l2.303-6.906a.5.5 0 0 1 .464-.322H7.5m4-7v5m-8 0V7a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v5m-8 0h6m-6 0a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2" />
+                            </svg>
+                            <span class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-red-500 rounded-full">
+            6
+        </span>
+
+                        </a>
+
 
                     </div>
                 </div>
